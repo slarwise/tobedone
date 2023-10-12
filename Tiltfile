fbbb11da9d1a79e489d87ed338062bb5a6bbe047
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2023-10-13 00:09:52 +0200

    Add redis database that holds the starting tasks

--- Tiltfile
+++ Tiltfile
@@ -6,4 +6,9 @@ docker_build("frontend", "./frontend")
 k8s_yaml("k8s/frontend.yaml")
 k8s_resource("frontend", port_forwards=8000)
 
+k8s_yaml("k8s/database.yaml")
+k8s_resource("database", port_forwards=6379)
+
+local_resource("initialize-database", cmd="kubectl exec deploy/database -- redis-cli SADD tasks:default '0:Read book' '1:Scroll instagram' '2:Eat food' '3:Drink water' '4:Take a nap' '5:1; DROP TABLE users' '6:Watch a movie'", resource_deps=["database"], allow_parallel=True)
+
 local_resource("items-docs", dir="./items", cmd="echo 'hej'", serve_cmd="make -C items docs", deps="./items/openapi.yaml", allow_parallel=True, links="http://localhost:8080")
--- items/src/main.rs
+++ items/src/main.rs
@@ -2,6 +2,7 @@ use axum::{
     extract::Path, http::header, http::HeaderMap, response::IntoResponse, routing::get, Json,
     Router,
 };
+use redis::Commands;
 use serde::Serialize;
 
 #[tokio::main]
@@ -18,11 +19,11 @@ async fn main() {
 #[derive(Serialize)]
 struct Item {
     value: String,
-    id: u32,
+    id: usize,
 }
 
 impl Item {
-    fn new(value: &str, id: u32) -> Item {
+    fn new(value: &str, id: usize) -> Item {
         Item {
             value: value.to_string(),
             id,
@@ -31,16 +32,19 @@ impl Item {
 }
 async fn get_items(Path(username): Path<String>) -> impl IntoResponse {
     println!("{}", username);
-    // TODO: Query the database for the user's items
-    let words: Vec<Item> = vec![
-        Item::new("Read book", 1),
-        Item::new("Scroll instagram", 2),
-        Item::new("Eat food", 3),
-        Item::new("Drink water", 4),
-        Item::new("Take a nap", 5),
-        Item::new("1; DROP TABLE users", 6),
-        Item::new("Watch a movie", 7),
-    ];
+    let client = redis::Client::open("redis://localhost:6379").unwrap();
+    let mut con = client.get_connection().unwrap();
+    let tasks: Vec<String> = con.smembers("tasks:default").unwrap();
+    let mut words: Vec<Item> = vec![];
+    for task in tasks {
+        match task.split_once(':') {
+            Some((id, task)) => {
+                let id: usize = id.parse().unwrap();
+                words.push(Item::new(&task, id));
+            }
+            None => panic!("Incorrect schema: database tasks must be on the form `<id>:<task>`"),
+        }
+    }
     let mut header_map = HeaderMap::new();
     header_map.insert(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*".parse().unwrap());
     (header_map, Json(words))
